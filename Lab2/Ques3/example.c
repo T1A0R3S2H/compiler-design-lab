a79f85fc52298841f175df6dfdb286c9bd983ad4
Tarsh Swarnkar <123285559+T1A0R3S2H@users.noreply.github.com>
2025-01-20 11:55:45 +0530

    Create example.c

---
+++ Lab2/Ques3/example.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    float b = 20.5;
+    if (a < b) {
+        printf("a is less than b\n");
+    } else {
+        printf("a is not less than b\n");
+    }
+    return 0;
+}
